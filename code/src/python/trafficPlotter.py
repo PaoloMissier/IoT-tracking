68bf6fd6a922ed3988da538f2e0a90b1397d2cf4
Paolo Missier <pmissier@acm.org>
2016-12-10 21:49:12 +0000

    adding support for multiple queries and plot refresh — in progress

--- code/src/python/trafficPlotter.py
+++ code/src/python/trafficPlotter.py
@@ -2,11 +2,12 @@
 
 import paho.mqtt.client as mqtt
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 import logging
 import mysql.connector
 import pandas as pd
 import matplotlib.pyplot as plt
-from dateutil import parser
+import time
 
 
 import numpy as np
@@ -30,10 +31,10 @@ DB_HOST = "localhost"
 DB_NAME='BrokerTracker'
 
 MIN_MAX_TS_QERY = "SELECT min(timestamp) as min, max(timestamp) as max FROM CONS"
-
 CNT_QUERY = "SELECT prodID, consID, topic, count(*) as cnt  FROM CONS C  where timestamp between date(%s) and date(%s) group by prodID, consID, topic"
 # DB_COLUMNS = [ "prodID", "consID",   "topic", "cnt"]
 
+REPLOT_INTERVAL = 5  #sec
 
 TOPIC_COUNT = 5
 TOPIC_ROOT="root/"
@@ -79,6 +80,26 @@ def genTopics(n):
 	return [ TOPIC_ROOT+str(i) for i in range(n)]
 
 
+## blocks if not enough records in the next window
+def getNextWindow(fromTS):
+
+	while True:
+		_, maxTS = DB_MinMax(cursor)  # get current latest timestamp
+
+		logger.info("available window size: {}".format(maxTS - fromTS))
+		soughtMaxTS = fromTS + relativedelta(seconds=REPLOT_INTERVAL) 
+		if soughtMaxTS <= maxTS:
+			logger.info("next window is complete. From {} to {}".format(fromTS, soughtMaxTS))
+			return soughtMaxTS
+		else:
+			logger.info("next window is INcomplete. From {} to {} but max is {}".format(fromTS, soughtMaxTS, maxTS))
+			time.sleep(REPLOT_INTERVAL)
+
+# 		print("mintS: {}, delta: {}, new maxTS: {}".format(minTS, REPLOT_INTERVAL, minTS + relativedelta(second=REPLOT_INTERVAL)))
+		
+
+
+
 ##
 # main
 ##	
@@ -90,155 +111,110 @@ if __name__ == '__main__':
 
 	# find min and max timestamps -- this is the largest window we can turn into a counter cube
 	minTS, maxTS = DB_MinMax(cursor)
-	logger.info("min: {}, max: {}".format(minTS, maxTS))
 
-	df  = DBRead(cursor, CNT_QUERY, [minTS, maxTS])
-	logger.info("loaded df with {}  records".format(len(df)))
+	firstWindow = True
 
-	# count number of producers  and consumers
-	prodCnt = len(df.groupby(['prodID']))
-	consCnt = len(df.groupby(['consID']))
-	logger.info("{} prod, {} cons".format(prodCnt, consCnt))
-
-	maxCnt = 0   # scale of Y axis is calibrated on max cnt across all groups
-	allTopics = list()   # xticks common to all plots
-	prodCnt = 0
-	maxConsCnt = 0
+	while True:
 	
-	gByProd = df.groupby('prodID') 		# groups with same (prod, cons)
-
-	plotGrid = list()  # of lists
-	i = 0
-	for prodID, prodDF in gByProd:
-	
-		gByCons = prodDF.groupby('consID')
-	
-		plotGrid.append(list())  # row of cells		
-
-		plotRow = plotGrid[i]
-		j = 0
-		for consID, consDF in gByCons:
+		logger.info("window: [{},{}]".format(minTS, maxTS))
+		maxTS = getNextWindow(minTS)
 		
-			# remove the index from the DF (??)
-			cnt  = [cnt for cnt in consDF['cnt']]  	## cnt are the values plotted on the bar			
-			topics = [topics  for topics in consDF['topic']]
+		df  = DBRead(cursor, CNT_QUERY, [minTS, maxTS])
+		logger.info("loaded df with {}  records".format(len(df)))
 
-			m = max(cnt)						
-			if m > maxCnt:
-				maxCnt = m				
+		# count number of producers  and consumers
+		prodCnt = len(df.groupby(['prodID']))
+		consCnt = len(df.groupby(['consID']))
+		logger.info("{} prod, {} cons".format(prodCnt, consCnt))
+
+		maxCnt = 0   # scale of Y axis is calibrated on max cnt across all groups
+		allTopics = list()   # xticks common to all plots
+		prodCnt = 0
+		maxConsCnt = 0
+	
+		gByProd = df.groupby('prodID') 		# groups with same (prod, cons)
+
+		plotGrid = list()  # of lists
+		i = 0
+		for prodID, prodDF in gByProd:
+	
+			gByCons = prodDF.groupby('consID')
+	
+			plotGrid.append(list())  # row of cells		
+
+			plotRow = plotGrid[i]
+			j = 0
+			for consID, consDF in gByCons:
+		
+				# remove the index from the DF (??)
+				cnt  = [cnt for cnt in consDF['cnt']]  	## cnt are the values plotted on the bar			
+				topics = [topics  for topics in consDF['topic']]
+
+				m = max(cnt)						
+				if m > maxCnt:
+					maxCnt = m				
 			
-			logger.debug("cell ({},{}) has\ntopics: {}\ncnt:{}".format(i,j,topics, cnt))
+				logger.debug("cell ({},{}) has\ntopics: {}\ncnt:{}".format(i,j,topics, cnt))
 
-			topicsCnt = {}
-			# create a topic -> cnt dict
-			for k in range(len(topics)):
-				topicsCnt[topics[k]] = cnt[k]
+				topicsCnt = {}
+				# create a topic -> cnt dict
+				for k in range(len(topics)):
+					topicsCnt[topics[k]] = cnt[k]
 						
-			for t in topics:
-				if t not in allTopics:
-					allTopics.append(t)
+				for t in topics:
+					if t not in allTopics:
+						allTopics.append(t)
 			
-			if len(cnt) > maxCnt:
-				maxCnt = len(cnt)
+				if len(cnt) > maxCnt:
+					maxCnt = len(cnt)
 			
-			plotRow.append((prodID, consID, topicsCnt))
-			j = j+1
-		i = i+1
+				plotRow.append((prodID, consID, topicsCnt))
+				j = j+1
+			i = i+1
 		
-	if maxConsCnt < j:
-		maxConsCnt = j
-	prodCnt = i
+		if maxConsCnt < j:
+			maxConsCnt = j
+		prodCnt = i
 	
-	logger.debug("plotting {}x{} grid".format(prodCnt, maxConsCnt))
+		logger.debug("plotting {}x{} grid".format(prodCnt, maxConsCnt))
 
+		if firstWindow: 
+			# init the plot grid
+			# create prodCnt x consCnt plots:
+			plt.ioff()  # ODD! should be on
 
-	## plot everything
-	# create prodCnt x consCnt plots:
-	fig, axes = plt.subplots(prodCnt, maxConsCnt, figsize=(30, 10))	
+			fig, axes = plt.subplots(prodCnt, maxConsCnt, figsize=(30, 10))	
 
-	## set properties across all plots
-	x_pos = np.arange(len(allTopics))			
-	logger.debug("x ticks pos for all plots: {}".format(x_pos))			
-	logger.debug("x axis labels: {}".format(allTopics))
+			## set properties across all plots
+			x_pos = np.arange(len(allTopics))			
+			logger.debug("x ticks pos for all plots: {}".format(x_pos))			
+			logger.debug("x axis labels: {}".format(allTopics))
 
-	plt.setp(axes, xticks=x_pos, xticklabels=allTopics, ylim=[0,maxCnt])
+		plt.setp(axes, xticks=x_pos, xticklabels=allTopics, ylim=[0,maxCnt])
 
-	for i in range(prodCnt):
-		for j in range(maxConsCnt):
+		for i in range(prodCnt):
+			for j in range(maxConsCnt):
 
-			(prodID, consID, topicsCnt) = plotGrid[i][j]
+				(prodID, consID, topicsCnt) = plotGrid[i][j]
 			
-			for t in allTopics:			
-				if t not in topicsCnt:
-					topicsCnt[t] = 0  			# set cnt = 0 for missing topics in this cell						
+				for t in allTopics:			
+					if t not in topicsCnt:
+						topicsCnt[t] = 0  			# set cnt = 0 for missing topics in this cell						
 
-			logger.debug("plot ({},{}) bar values: {}".format(i,j,topicsCnt.values()))
+				logger.debug("plot ({},{}) bar values: {}".format(i,j,topicsCnt.values()))
 
-			# add prodID, consID to legend / title
-			axes[i,j].bar(x_pos, topicsCnt.values(), alpha=0.2, color='k')
-			axes[i,j].set_title("P={}/C={}".format(prodID,consID))
-	plt.show()
+				# add prodID, consID to legend / title
+				axes[i,j].bar(x_pos, topicsCnt.values(), alpha=0.2, color='k')
+				axes[i,j].set_title("P={}/C={}".format(prodID,consID))
 
-# 	topicList = genTopics(TOPIC_COUNT)
-# 	
-# 	## topics are x axis labels
-# 	allTopics =  topicList
-# 	print(allTopics)
-# 	x_pos = np.arange(len(allTopics))			
-# 	print(x_pos)			
-# 
-# 	plt.setp(axes, xticks=x_pos, xticklabels=allTopics)
-# 	
-# 	# groups with same (prod, cons)
-# 	groupedByProd = df.groupby('prodID')
-# 
-# 	# each group needs further grouping by 'consID'	
-# 	i = 0
-# 	prodConsGroup = []
-# 	for prodID, prodDF in groupedByProd:
-# 		prodGroup = {}
-# 		groupedByCons = prodDF.groupby('consID')
-# 		j = 0
-# 		for consID, consDF in groupedByCons:
-# 			prodGroup[consID] = consDF
-# 			
-# 			## cnt are the values plotted on the bar
-# 			cnt  = [cnt for cnt in consDF['cnt']]
-# 			topics = [topics  for topics in consDF['topic']]
-# 			
-# 			# set cnt = 0 for missing topics			
-# 			actualCnt = []
-# 			for k in range(len(allTopics)):
-# 				if allTopics[k] in topics:
-# 					actualCnt.append(cnt[i])
-# 				else:
-# 					actualCnt.append(0)
-# 
-# 			print("****\nx_pos: {}".format(x_pos))
-# 			print("these topics: {}".format(topics))
-# 			print("cnt: {}".format(cnt))
-# 			print("actualCnt: {}".format(actualCnt))
-# 			
-# # 			print("***\n plot ({},{}):\n topics: {} \n cnt: {}\n***".format(i,j,topics, cnt))
-# 			j = j+1
-# 			
-# 		prodConsGroup[prodID] = prodGroup
-# 		i = i+1
+		plt.show()
+		print("are we here?")
+		fig.canvas.draw()
+		firstWindow = False
 
-
-# 
-# 	objects = ('Python', 'C++', 'Java', 'Perl', 'Scala', 'Lisp')
-# 	y_pos = np.arange(len(objects))
-# 	performance = [10,8,6,4,2,1]
-#  
-# 	plt.barh(y_pos, performance, align='center', alpha=0.5)
-# 	plt.yticks(y_pos, objects)
-# 	plt.xlabel('Usage')
-# 	plt.title('Programming language usage')
-#  
-# 	plt.show()	
-	
-	
+		print("are we here yet?")
+		
+		minTS = maxTS
 	
 	
 	
