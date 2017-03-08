114550f952817e4dfd26256bd38251069e14d63d
phantomlinux <vaughnchong@gmail.com>
2017-03-08 20:16:30 +0000

    Update settings.py

--- VAUGHN/webapp/webapp/settings.py
+++ VAUGHN/webapp/webapp/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'bj8$!xk9#rr$3=fq&v*0-^sq@n)fczdo%3o%%zl-43bqt3&sby'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [0.0.0.0]
 
 
 # Application definition
